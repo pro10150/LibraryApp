a83f9d73c8401fcdf3f98892abbc034650f7eece
pro10150 <67267679+pro10150@users.noreply.github.com>
2020-11-14 00:14:29 +0700

    Update DBVars.java

--- src/Backdoor/DBVars.java
+++ src/Backdoor/DBVars.java
@@ -3,9 +3,9 @@ package Backdoor;
 
 public class DBVars {
     //Por's Passoword
-    //public static String DBPassword = "@Pro3083711"; // Not needed.
+    public static String DBPassword = "@Pro3083711"; // Not needed.
     //Job's Password
-    public static String DBPassword = "!KdHwE@WRUAU#uon$2";
+    //public static String DBPassword = "!KdHwE@WRUAU#uon$2";
     
     //Arm's Password
     //public static String DBPassword = "";
