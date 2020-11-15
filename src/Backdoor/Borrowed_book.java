84d6d745db03cb3895a5e547f6dcc6c9862c4684
Noppakun A. <pro10150@gmail.com>
2020-11-16 01:10:13 +0700

    Add methods for UserPickBook, ReservedBook and add some changes to Borrowedbook. Will tell if there the borrow request is complete through isBorrow. Same goes with ReservedBook

--- src/Backdoor/Borrowed_book.java
+++ src/Backdoor/Borrowed_book.java
@@ -9,42 +9,62 @@ public class Borrowed_book extends Member{
     private int remaining;
     private Date dueDate;
     private Date startDate;
-    public static Statement statement = null;
-    public static ResultSet resultSet = null;
-    public static String url = "jdbc:mysql://localhost:3306/library";
-    public static String user = "root",pass = DBVars.DBPassword;
-    public Connection connect = DriverManager.getConnection(url,user,pass);
+    private Date returnDate;
+    private boolean lateCheck;
+    private boolean isBorrow;
+
+    public Borrowed_book(String user_ID) throws SQLException{
+        this.user_ID = user_ID;
+        statement = connect.createStatement();
+        resultSet = statement.executeQuery("SELECT * FROM borrowed_book");
+
+        while (resultSet.next()){
+            if (this.user_ID.equals(resultSet.getString("user_ID"))){
+                this.book_ID = resultSet.getString("book_ID");
+                this.startDate = resultSet.getDate("start_date");
+                this.dueDate = resultSet.getDate("due_date");
+                this.returnDate = resultSet.getDate("start_date");
+                this.lateCheck = resultSet.getBoolean("late_check");
+                //System.out.println(toString());
+            }
+
+        }
+    }
 
     public Borrowed_book(String user_ID, String book_ID) throws SQLException{
         this.user_ID = user_ID;
         this.book_ID = book_ID;
         PhysicalBook book = new PhysicalBook(book_ID);
         this.remaining = book.getRemaining();
-        String query = "INSERT INTO borrowed_book(user_ID,book_ID,start_date,due_date) VALUES (?, ?, ?, ?)";
-        Calendar calendar = Calendar.getInstance();
-        java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
 
-        long date = System.currentTimeMillis() + 14 * 24 * 3600 * 1000;
-        Date dueDate = new Date(date);
-        this.dueDate = dueDate;
+        if (this.remaining == 0) isBorrow = false;
+        else{
+            String query = "INSERT INTO borrowed_book(user_ID,book_ID,start_date,due_date) VALUES (?, ?, ?, ?)";
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+            this.startDate = startDate;
 
-        PreparedStatement preparedStatement = connect.prepareStatement(query);
-        preparedStatement.setString(1,user_ID);
-        preparedStatement.setString(2,book_ID);
-        preparedStatement.setDate(3,startDate);
-        preparedStatement.setDate(4,this.dueDate);
-        preparedStatement.execute();
+            long date = System.currentTimeMillis() + 14 * 24 * 3600 * 1000;
+            Date dueDate = new Date(date);
+            this.dueDate = dueDate;
 
-        book.reduceRemaining();
-        book.updateRemaining(this.remaining--);
+            PreparedStatement preparedStatement = connect.prepareStatement(query);
+            preparedStatement.setString(1,this.user_ID);
+            preparedStatement.setString(2,this.book_ID);
+            preparedStatement.setDate(3,this.startDate);
+            preparedStatement.setDate(4,this.dueDate);
+            preparedStatement.execute();
+
+            book.reduceRemaining();
+            book.updateRemaining(this.remaining--);
+            isBorrow = true;
+        }
     }
-    
-    
-   
 
     public void returnBook() throws SQLException {
         Calendar calendar = Calendar.getInstance();
-        java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+        java.sql.Date returnDate = new java.sql.Date(calendar.getTime().getTime());
+        this.returnDate = returnDate;
 
         boolean late_check = false;
 
@@ -62,11 +82,32 @@ public class Borrowed_book extends Member{
         String query = "UPDATE borrowed_book SET return_date = ?, late_check = ? WHERE user_ID = ? AND book_ID = ? " +
                 "AND due_date = ?";
         PreparedStatement preparedStatement = connect.prepareStatement(query);
-        preparedStatement.setDate(1,startDate);
+        preparedStatement.setDate(1,this.returnDate);
         preparedStatement.setBoolean(2,late_check);
         preparedStatement.setString(3,this.user_ID);
         preparedStatement.setString(4,this.book_ID);
         preparedStatement.setDate(5,this.dueDate);
         preparedStatement.execute();
+    }
+
+    public Date getDueDate(){
+        return dueDate;
+    }
+
+    public Date getStartDate(){
+        return startDate;
+    }
+
+    public Date getReturnDate(){
+        return returnDate;
+    }
+
+    public boolean getLateCheck(){
+        return lateCheck;
+    }
+
+    public String toString(){
+        return "bookid = " + book_ID + "\nstartdate = " + startDate + "\nduedate = " + dueDate + "\nreturndate = "
+                + returnDate + "\nlatecheck = " + lateCheck;
     }
 }
